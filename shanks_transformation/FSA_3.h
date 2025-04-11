ff41efd223799bc0a974c880c507969c5a74435f
Panama <kiri4sharonov@mail.ru>
2025-04-11 19:21:41 +0300

    fix N_max

--- shanks_transformation/FSA_3.h
+++ shanks_transformation/FSA_3.h
@@ -15,17 +15,27 @@ template <typename T, typename K, typename series_templ>
 class ford_sidi_algorithm_three : public series_acceleration<T, K, series_templ>{
 public:
 
-    /*    * @brief Parameterized constructor to initialize the Ford-Sidi V-3 Algorithm.
-    * @authors Sharonov K.S.    * @param series The series class object to be accelerated
-    */    ford_sidi_algorithm_three(const series_templ& series);
-    /*
+    /** @brief Parameterized constructor to initialize the Ford-Sidi V-3 Algorithm.
+      * @authors Sharonov K.S.    
+      * @param series The series class object to be accelerated
+    */    
+    ford_sidi_algorithm_three(const series_templ& series, K n);
+
+    /** 
     * @brief Fast implementation of Ford-Sidi.    
     * @param n The number of terms in the partial sum.
     * @param order The order of transformation.    
-    * * @return The partial sum after the transformation.
-    */    T operator()(const K n, const int k) const;
+    * @return The partial sum after the transformation.
+    */    
+    T operator()(const K n, const int k) const;
+
 protected:
-    mutable std::vector<std::vector<T>> FSG; // mutable allows modification in const methods
+    /** 
+     * @brief The value(n), that user input.    
+     * @authors Sharonov K.S.
+     */
+    K Nmax = 0;
+    mutable std::vector<std::vector<T>> FSG; 
     mutable std::vector<T> FSA;
     mutable std::vector<T> FSI;
     mutable std::vector<T> G;
@@ -33,8 +43,7 @@ protected:
 
 
 template <typename T, typename K, typename series_templ>
-ford_sidi_algorithm_three<T, K, series_templ>::ford_sidi_algorithm_three(const series_templ& series) : series_acceleration<T, K, series_templ>(series), FSG(1002, std::vector<T>(1001, 0.0)), FSA(1001, 0.0), FSI(1001, 0.0), G(1002, 0.0) {}
-
+ford_sidi_algorithm_three<T, K, series_templ>::ford_sidi_algorithm_three(const series_templ& series, K n) :series_acceleration<T, K, series_templ>(series), FSG(n + 2, std::vector<T>(n + 1, 0.0)), FSA(n + 1, 0.0), FSI(n + 1, 0.0), G(n + 2, 0.0), Nmax(n) {}
 
 template <typename T, typename K, typename series_templ>
 T ford_sidi_algorithm_three<T, K, series_templ>::operator()(const K n, const int order) const{
@@ -43,7 +52,7 @@ T ford_sidi_algorithm_three<T, K, series_templ>::operator()(const K n, const int
     
     T Te = 1.0 / (n);
     
-    for (int k = 1; k <= n + 500; k++) {
+    for (K k = 1; k <= this->Nmax + 1; k++) {
         if (k == 1) {
             G[k] = G[k] * pow(n, k);
         } else {
@@ -54,27 +63,27 @@ T ford_sidi_algorithm_three<T, K, series_templ>::operator()(const K n, const int
     if (abs(G[1]) >= 1e-77) {
         FSA[n - 1] = this -> series -> S_n(n-1) / G[1];
         FSI[n - 1] = 1.0 / G[1];
-        for (int i = 2; i <= n+500; i++) {
+        for (int i = 2; i <= this->Nmax + 1; i++) {
             FSG[i][n - 1] = G[i] / G[1];
         }
     } else {
         FSA[n - 1] = this -> series -> S_n(n-1);
         FSI[n - 1] = 1.0;
-        for (int i = 2; i <= n+500; i++) {
+        for (int i = 2; i <= Nmax + 1; i++) {
             FSG[i][n - 1] = G[i];
         }
     }
 
     // Основной цикл алгоритма Ford-Sidi
-    for (int k = 0; k <= n - 1; k++) {
+    for (K k = 0; k <= n - 1; k++) {
         K MM = n - (k + 1) - 1;
         T D = FSG[k + 2][MM + 1] - FSG[k + 2][MM];
-
-        for (int i = k + 3; i <= n+500; i++) {
+                                                    
+        for (K i = k + 3; i <= this->Nmax - 3; i++) {
             FSG[i][MM] = (FSG[i][MM + 1] - FSG[i][MM]) / D;
         }
-            FSA[MM] = (FSA[MM + 1] - FSA[MM]) / D;
-            FSI[MM] = (FSI[MM + 1] - FSI[MM]) / D;
+        FSA[MM] = (FSA[MM + 1] - FSA[MM]) / D;
+        FSI[MM] = (FSI[MM + 1] - FSI[MM]) / D;                                                                                                                            
 
     }
     return FSA[0] / FSI[0];
