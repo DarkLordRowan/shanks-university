37cba72817776daed454523fc484adce95331050
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-25 23:34:23 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/algorithm/weniger_algorithm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+template<typename T, typename K, typename series_templ>
+class weniger_algorithm final: public series_acceleration<T, K, series_templ> {
+public:
+    explicit weniger_algorithm(const series_templ &series);
+
+    T operator()(K n, int order) const override;
+};
+
+#include "weniger_algorithm.tpp"
