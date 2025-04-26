23311e243f64cc15111f355aeefc8cde67f5a059
AlexMax37 <136169394+AlexMax37@users.noreply.github.com>
2025-04-26 12:55:53 +0300

    Delete sedogbo_acceleration.h

--- shanks_transformation/sedogbo_acceleration.h
+++
@@ -1,9 +0,0 @@
-
-    /**
-     * @brief Комбинированный алгоритм
-     * @param terms Члены последовательности
-     * @param iter Количество итераций
-     */
-    std::vector<T> combined_algorithm(const std::vector<T>& terms, K iter) {
-        
-    }
