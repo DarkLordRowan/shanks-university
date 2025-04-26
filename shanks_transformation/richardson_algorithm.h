2ac6219c49b86310bb3efc8a821e028d6dca53cf
AlexMax37 <136169394+AlexMax37@users.noreply.github.com>
2025-04-26 12:51:11 +0300

    добавлены ускорения какого-то sedogbo
    вкратце: НИЧЕГО НЕ РАБОАЕТ
    
    ===test_framework.h===
    добавил 3 алгоса
    "epsilon_modified_algorithm.h"
    "theta_modified_algorithm.h"
    "epsilon_aitken_theta_algorithm.h"
    
    - я бы их удалил конечно, но их проблемы
    
    1. у epsilon_modified_algorithm.h чекнуть комментарий на значение k1 - 1
    2.  epsilon_modified_algorithm.h бредит
    3. theta_modified_algorithm.h бредит
    4. epsilon_aitken_theta_algorithm.h бредит

--- shanks_transformation/richardson_algorithm.h
+++ shanks_transformation/richardson_algorithm.h
@@ -24,53 +24,46 @@ public:
       * @brief Parameterized constructor to initialize the Richardson transformation for series.
       * @param series The series class object
       */
-    richardson_algorithm(const series_templ& series);
+    richardson_algorithm(const series_templ& series) : series_acceleration<T, K, series_templ>(series) {}
      /**
       * @brief Richardson transformation for series function.
       * @param n The number of terms in the partial sum.
       * @param order The order of transformation.
       * @return The partial sum after the transformation.
       */
-    T operator() (const K n, const int order) const;
-};
+    T operator() (const K n, const int order) const {
+        // in the method we don't use order, it's only a stub 
+        if (n < 0)
+            throw std::domain_error("negative integer in the input");
 
-template <typename T, typename K, typename series_templ>
-richardson_algorithm<T, K, series_templ>::richardson_algorithm(const series_templ& series) : series_acceleration<T, K, series_templ>(series) {}
+        if (n == 0)
+            return DEF_UNDEFINED_SUM;
 
-template <typename T, typename K, typename series_templ>
-T richardson_algorithm<T, K, series_templ>::operator()(const K n, const int order) const
-{
-    // in the method we don't use order, it's only a stub 
-    if (n < 0)
-        throw std::domain_error("negative integer in the input");
+        std::vector<std::vector<T>> e(2, std::vector<T>(n + 1, 0)); //2 vectors n + 1 length containing Richardson table next and previous 
 
-    if (n == 0)
-        return DEF_UNDEFINED_SUM;
+        for (int i = 0; i <= n; ++i)
+            e[0][i] = this->series->S_n(i);
 
-    std::vector<std::vector<T>> e(2, std::vector<T>(n + 1, 0)); //2 vectors n + 1 length containing Richardson table next and previous 
+        // The Richardson method main function 
+        T a, b;
+        a = static_cast<T>(1);
 
-    for (int i = 0; i <= n; ++i) 
-       e[0][i] = this->series->S_n(i);
+        for (int l = 1; l <= n; ++l) {
+            a *= 4;
+            b = a - 1;
+            for (int m = l; m <= n; ++m)
+                e[1][m] = (a * e[0][m] - e[0][m - 1]) / b;
 
-    // The Richardson method main function 
-    T a, b;
-    a = static_cast<T>(1);
-    
-    for (int l = 1; l <= n; ++l) {
-        a *= 4;
-        b = a - 1;
-        for (int m = l; m <= n; ++m) 
-            e[1][m] = (a * e[0][m] - e[0][m - 1]) / b;
-        
-        std::swap(e[0], e[1]);
+            std::swap(e[0], e[1]);
+        }
+
+        // n & 1 gives first bit 
+
+        const T res = e[n & 1][n]; // get n & 1, cause if n is even, result is e[0][n], if n is odd, result is e[1][n]
+
+        if (!std::isfinite(res))
+            throw std::overflow_error("division by zero");
+
+        return res;
     }
-       
-    // n & 1 gives first bit 
-
-    const T res = e[n & 1][n]; // get n & 1, cause if n is even, result is e[0][n], if n is odd, result is e[1][n]
-
-    if (!std::isfinite(res))
-        throw std::overflow_error("division by zero");
-
-    return res;
-}
+};
---
+++ shanks_transformation/sedogbo_acceleration.h
@@ -0,0 +1,9 @@
+
+    /**
+     * @brief Комбинированный алгоритм
+     * @param terms Члены последовательности
+     * @param iter Количество итераций
+     */
+    std::vector<T> combined_algorithm(const std::vector<T>& terms, K iter) {
+        
+    }
