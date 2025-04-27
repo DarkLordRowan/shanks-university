4ab88073ee863046bbf519f661257e5bf69e8f8f
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-27 09:44:27 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/algorithm/epsilon_three_algorithm.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+template<typename T, typename K, typename series_templ>
+class epsilon_three_algorithm final : public series_acceleration<T, K, series_templ> {
+    T epsilon_threshold;
+
+public:
+    explicit epsilon_three_algorithm(const series_templ &series, T epsilon_threshold_ = T(1e-3));
+
+    T operator()(K n, int order) const override;
+};
+
+#include "epsilon_three_algorithm.tpp"
