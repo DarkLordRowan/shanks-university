2a2d368b327ea1bb6838a74b6aa8cc7ba4d14bce
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-27 09:47:34 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/algorithm/epsilon_two_algorithm.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+template<typename T, typename K, typename series_templ>
+class epsilon_two_algorithm final: public series_acceleration<T, K, series_templ> {
+public:
+    explicit epsilon_two_algorithm(const series_templ &series);
+
+    T operator()(K n, int order) const override;
+};
+
+#include "epsilon_two_algorithm.tpp"
