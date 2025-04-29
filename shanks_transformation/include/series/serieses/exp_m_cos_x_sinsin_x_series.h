eeb50ec6c2dca45c32c9af7d95c9ecff424ea9a1
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-29 20:44:09 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/series/serieses/exp_m_cos_x_sinsin_x_series.h
@@ -0,0 +1,47 @@
+
+
+/**
+* @brief Maclaurin series of exp(-cos(x)) * sin(sin(x))
+* @authors Pashkov B.B.
+* @tparam T The type of the elements in the series, K The type of enumerating integer
+*/
+
+#pragma once
+
+#include <cmath>
+
+#include "../series_base.h"
+#include "../recurrent_series_base.h"
+
+template <typename T, typename K>
+class exp_m_cos_x_sinsin_x_series final : public series_base<T, K>
+{
+public:
+    exp_m_cos_x_sinsin_x_series() = delete;
+
+    /**
+    * @brief Parameterized constructor to initialize the series with function argument and sum
+    * @authors Pashkov B.B.
+    * @param x The argument for function series
+    */
+    exp_m_cos_x_sinsin_x_series(T x);
+
+    /**
+    * @brief Computes the nth term of the exp(-cos(x)) * sin(sin(x)) series
+    * @authors Pashkov B.B.
+    * @param n The number of the term
+    * @return nth term of the series
+    */
+    [[nodiscard]] T operator()(K n) const override;
+};
+
+template <typename T, typename K>
+exp_m_cos_x_sinsin_x_series<T, K>::exp_m_cos_x_sinsin_x_series(T x) : series_base<T, K>(x, std::exp(-std::cos(x))* std::sin(std::sin(x))) {}
+
+template <typename T, typename K>
+T exp_m_cos_x_sinsin_x_series<T, K>::operator()(K n) const
+{
+    if (n < 0)
+        throw std::domain_error("negative integer in the input");
+    return this->minus_one_raised_to_power_n(n) * std::sin(n * this->x) / this->fact(n);
+}
---
+++ shanks_transformation/include/series/serieses/pi_six_min_half_series.h
@@ -0,0 +1,46 @@
+
+/**
+* @brief pi/5 - 1/2 series p 534
+* @authors Kreynin R.G.
+* @tparam T The type of the elements in the series, K The type of enumerating integer
+*/
+#pragma once
+
+#include <cmath>
+
+#include "../series_base.h"
+#include "../recurrent_series_base.h"
+
+template <typename T, typename K>
+class pi_six_min_half_series final : public series_base<T, K>
+{
+public:
+    pi_six_min_half_series() = delete;
+
+    /**
+    * @brief Parameterized constructor to initialize the series with function argument and sum
+    * @authors Kreynin R.G.
+    * @param x The argument for function series
+    */
+    explicit pi_six_min_half_series(T x);
+
+    /**
+    * @brief Computes the nth term of the Maclaurin series of the sine function
+    * @authors Kreynin R.G.
+    * @param n The number of the term
+    * @return nth term of the Maclaurin series of the sine functions
+    */
+    [[nodiscard]] T operator()(K n) const override;
+};
+
+template <typename T, typename K>
+pi_six_min_half_series<T, K>::pi_six_min_half_series(T x) : series_base<T, K>(x, static_cast<T>(x* (std::numbers::pi / 6 - 1 / 2))) {}
+
+template <typename T, typename K>
+T pi_six_min_half_series<T, K>::operator()(K n) const
+{
+    if (n < 0)
+        throw std::domain_error("negative integer in the input");
+    return this->x * series_base<T, K>::minus_one_raised_to_power_n(n) / ((6 * n + 5) * (6 * n + 7));
+}
+
