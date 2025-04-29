0765e0ec38d77ec5b890152916a5c726682cc19b
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-29 13:56:33 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/algorithm/richardson_algorithm.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#define DEF_UNDEFINED_SUM 0
+
+
+template<typename T, typename K, typename series_templ>
+class richardson_algorithm : public series_acceleration<T, K, series_templ> {
+public:
+    // Конструктор
+    richardson_algorithm(const series_templ &series);
+
+    // Оператор вызова
+    T operator()(const K n, const int order) const;
+};
+
+#include "richardson_algorithm.tpp"
