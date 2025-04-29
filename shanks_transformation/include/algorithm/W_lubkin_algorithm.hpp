d98fb2ef07038380886c905d32a11f0f84e771a8
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-29 13:48:53 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/algorithm/W_lubkin_algorithm.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+
+template<typename T, typename K, typename series_templ>
+class W_lubkin_algorithm : public series_acceleration<T, K, series_templ> {
+protected:
+    // Метод для вычисления
+    T calculate(K n, const int order, T S_n, const K j) const;
+
+public:
+    // Конструктор
+    W_lubkin_algorithm(const series_templ &series);
+
+    // Оператор вызова
+    T operator()(const K n, const int order) const;
+};
+
+#include "W_lubkin_algorithm.tpp"
