0070068154cb5302b4b4fa65e331db2e7645ee7f
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-04-29 13:32:42 +0300

    Cleanup. Cleanup

---
+++ shanks_transformation/include/algorithm/levin_sidi_S_algorithm.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+template<typename T, typename K, typename series_templ>
+class levin_sidi_S_algorithm : public series_acceleration<T, K, series_templ> {
+protected:
+    const T beta;
+    const transform_base<T, K> *remainder_func;
+    bool recursive;
+
+    virtual T calculate(const K &n, const int &order) const;
+    T calculate_rec(const K &n, const int &order) const;
+
+public:
+    levin_sidi_S_algorithm(const series_templ &series, const transform_base<T, K> *func, bool recursive = false,
+                           const T beta_ = T(1));
+
+    ~levin_sidi_S_algorithm();
+
+    T operator()(const K n, const int order) const;
+};
+
+#include "levin_sidi_S_algorithm.tpp"
