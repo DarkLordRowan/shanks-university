908ef874c7d604badf773f32a7cdd92a037e8d32
dankekw <danila.vovf@yandex.ru>
2025-05-03 12:21:20 +0300

    67 fix, 70, 69, 68, 59 checked

--- shanks_transformation/series.h
+++ shanks_transformation/series.h
@@ -3201,9 +3201,9 @@ constexpr T Riemann_zeta_func_xmin1_div_Riemann_zeta_func_x_series<T, K>::operat
 
 
 /**
-* @brief Taylor series of function ((x^2 + 3) / (x^2 + 2*x)) - 1
+* @brief Taylor series of function ((x^2 + 3) / (x^2 + 2*x)) - 1 at x = 1
 * @authors Trudolyubov N.A.
-* @tparam T The type of the elements in the series, K The type of enumerating integer
+* @tparam T - type of the elements in the series, K - type of enumerating integer
 */
 template <typename T, typename K>
 class xsquareplus3_div_xsquareplus2multix_minus_1_series : public series_base<T, K>
@@ -3214,30 +3214,30 @@ public:
 	/**
 	* @brief Parameterized constructor to initialize the series with function argument and sum
 	* @authors Trudolyubov N.A.
-	* @param x The argument for function series
+	* @param x - argument for function series
 	*/
 	xsquareplus3_div_xsquareplus2multix_minus_1_series(T x);
 
 	/**
 	* @brief Computes the nth term of the Taylor series of the sine function
 	* @authors Trudolyubov N.A.
-	* @param n The number of the term
-	* @return nth term of the Taylor series of the sine functions
+	* @param n - number of term
+	* @return nth term of Taylor series of the sine functions
 	*/
 	[[nodiscard]] constexpr virtual T operator()(K n) const;
 };
 
 template <typename T, typename K>
-xsquareplus3_div_xsquareplus2multix_minus_1_series<T, K>::xsquareplus3_div_xsquareplus2multix_minus_1_series(T x) : series_base<T, K>(x, (x* x + 3) / (x * (x + 2)) - 1) {}
+xsquareplus3_div_xsquareplus2multix_minus_1_series<T, K>::xsquareplus3_div_xsquareplus2multix_minus_1_series(T x) : series_base<T, K>(x, ((x * x + 3) / x * (x + 2)) - 1) {}
 
 template <typename T, typename K>
 constexpr T xsquareplus3_div_xsquareplus2multix_minus_1_series<T, K>::operator()(K n) const
 {
 	if (n < 0)
 		throw std::domain_error("negative integer in the input");
-	if (std::abs(this->x - 1) <= 3)
-		throw std::domain_error("The absolute value of x - 1 must be greater than 3");
-	return static_cast<T>(std::pow(-1, n) * (1.5 - 3.5 * std::pow(3, n)) / std::pow(this->x - 1, n + 1));
+	if (std::abs(this->x - 1) >= 2)
+		throw std::domain_error("The absolute value of x - 1 must be lower than 2");
+	return static_cast<T>(0.5 * series_base<T, K>::minus_one_raised_to_power_n(n) * std::pow(3, -n - 1) * (std::pow(3, n + 2) - 7) * std::pow(this->x - 1, n));
 }
 
 
