aa3675c171025e4896da7018579823037a79627e
Alexandr Naumov <alex2004naumov@gmail.com>
2025-09-08 15:54:58 +0300

    по тормазам

--- shanks_transformation/methods/FSA.hpp
+++ shanks_transformation/methods/FSA.hpp
@@ -59,4 +59,66 @@ public:
 	T operator()(const K n, const K k) const override;
 };
 
-#include "FSA.tpp"
+template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+T ford_sidi_algorithm<T, K, series_templ>::Psi(
+	const K n, 
+	const K k, 
+	const series_base<T,K>* u, 
+	const shanks_transform<T, K, series_templ>* g
+) const {
+	if (k == static_cast<K>(0))
+		return (u->operator()(n)) / (g->operator()(n, static_cast<K>(1)));
+	
+	K n1  = n + static_cast<K>(1);
+	K km1 = k - static_cast<K>(1);
+	K kp1 = k + static_cast<K>(1);
+	return (Psi(n1, km1, u, g) - Psi(n, km1, u, g)) / (Psi(n1, km1, kp1, g) - Psi(n, km1, kp1, g));
+}
+
+template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+T ford_sidi_algorithm<T, K, series_templ>::Psi(
+	const K n, 
+	const K k, 
+	const K k_1, 
+	const shanks_transform<T, K, series_templ>* g
+) const {
+	if (k == static_cast<K>(0))
+		return (g->operator()(n, k_1)) / (g->operator()(n, static_cast<K>(1)));
+	
+	K n1  = n + static_cast<K>(1);
+	K km1 = k - static_cast<K>(1);
+	K kp1 = k + static_cast<K>(1);
+
+	return (Psi(n1, km1, k_1, g) - Psi(n, km1, k_1, g)) / (Psi(n1, km1, kp1, g) - Psi(n, km1, kp1, g));
+}
+
+template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+T ford_sidi_algorithm<T, K, series_templ>::operator()(const K n, const K k) const 
+{
+	std::unique_ptr<one_series<T, K>> ones_seq(new one_series<T,K>(1));
+	
+	// Насколько мы коллективным разумом поняли, g это как бы ряд используемого преобразования(shanks_transform, richardson_extrapolation, G_transformation и тд)
+	// пока я не реализовал выбор и оставил только shanks_transform, хотя в дальнейшем выбор конечно же надо прикрутить
+	
+	K k1 = k - static_cast<K>(1);
+	K i1;
+	T T_n_k = Psi(static_cast<K>(2), k1, (this->series), shanks_trans.get()) - Psi(static_cast<K>(1), k1, (this->series), shanks_trans.get());
+	T_n_k /= (Psi(static_cast<K>(2), k1, ones_seq.get(), shanks_trans.get()) - Psi(static_cast<K>(1), k1, ones_seq.get(), shanks_trans.get()));
+	
+	for (K i = static_cast<K>(2); i <= n; ++i)
+	{
+		i1 = i + static_cast<K>(1);
+		T a = Psi(i1, k1, (this->series), shanks_trans.get()) - Psi(i, k1, (this->series), shanks_trans.get());
+		a /= (Psi(i1, k1, ones_seq.get(), shanks_trans.get()) - Psi(i, k1, ones_seq.get(), shanks_trans.get()));
+		T_n_k += a;
+	}
+
+	return T_n_k;
+}
+
+template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+ford_sidi_algorithm<T, K, series_templ>::ford_sidi_algorithm(const series_templ& series) : series_acceleration<T, K, series_templ>(series)
+{
+	shanks_trans.reset(new shanks_transform<T, K, series_templ>(this->series));
+}
+
--- shanks_transformation/methods/richardson_algorithm.hpp
+++ shanks_transformation/methods/richardson_algorithm.hpp
@@ -33,4 +33,44 @@ public:
     T operator() (const K n, const K order) const override;
 };
 
-#include "richardson_algorithm.tpp"
+template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+T richardson_algorithm<T, K, series_templ>::operator()(const K n, const K order) const {
+
+    using std::isfinite;
+    using std::fma;
+
+    // in the method we don't use order, it's only a stub 
+    if (n == static_cast<K>(0)) 
+        throw std::domain_error("n = 0 in the input");
+
+    std::vector<std::vector<T>> e(
+        2,
+        std::vector<T>(
+            n + static_cast<K>(1),
+            static_cast<T>(0)
+        )
+    ); //2 vectors n + 1 length containing Richardson table next and previous 
+    
+    for (K i = static_cast<K>(0); i <= n; ++i)
+        e[0][i] = this->series->S_n(i);
+
+    // The Richardson method main function 
+    T a, b;
+    a = static_cast<T>(1);
+    for (K l = static_cast<K>(1); l <= n; ++l) {
+        a *= static_cast<T>(4);
+        b = a - static_cast<T>(1);
+
+        for (K m = l; m <= n; ++m){
+            e[1][m] = fma(a, e[0][m], -e[0][m - static_cast<K>(1)]); 
+            e[1][m]/= b;
+        }
+
+        std::swap(e[0], e[1]);
+    }
+
+    if (!isfinite(e[n & static_cast<K>(1)][n])) // get n & 1, cause if n is even, result is e[0][n], if n is odd, result is e[1][n]
+        throw std::overflow_error("division by zero");
+
+    return e[n & static_cast<K>(1)][n];
+}
