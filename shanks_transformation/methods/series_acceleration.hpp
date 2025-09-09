f0f4747c2e0ef7c5d271fe488c48d63f14cdf0fd
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-09-09 11:41:40 +0300

    CPP. Cleanup

--- shanks_transformation/methods/series_acceleration.hpp
+++ shanks_transformation/methods/series_acceleration.hpp
@@ -1,16 +1,15 @@
 /**
- * @file series_acceleration.h
+ * @file series_acceleration.hpp
  * @brief This file contains the declaration of the base class series_acceleration
  */
 
 #pragma once
- /** @brief Default value for undefined transformation */
+/** @brief Default value for undefined transformation */
 #define DEF_NO_TRANSFORM 0
 /** @brief Default value for an unspecified series */
 #define NO_SERIES_GIVEN 0
 
 #include <iostream>   // Include the iostream library for I/O functionalities
-#include <math.h>     // Include the math library for mathematical functions
 
 
 /**
@@ -21,47 +20,47 @@
  * the partial sum after transformation is used, and the difference between the latter and the former.
  * @tparam T The type of the elements in the series, K The type of enumerating integer, series_templ is the type of series whose convergence we accelerate
  */
-template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+template<std::floating_point T, std::unsigned_integral K, typename series_templ>
 class series_acceleration
 {
 public:
-	virtual ~series_acceleration() = default;
+    virtual ~series_acceleration() = default;
 
-	/**
-   * @brief Parameterized constructor to initialize the Transformation.
-   * @authors Bolshakov M.P.
-   * @param series The series class object to be accelerated
-   */
-	series_acceleration(const series_templ& series);
+    /**
+     * @brief Parameterized constructor to initialize the Transformation.
+     * @authors Bolshakov M.P.
+     * @param series The series class object to be accelerated
+     */
+    explicit series_acceleration(const series_templ &series);
 
-	/**
+    /**
    * @brief Method for printing out the info about the object of this class
    * @authors Bolshakov M.P.
    */
-	constexpr void print_info() const;
+    constexpr void print_info() const;
 
-	/**
+    /**
    * @brief Virtual operator() that returns the partial sum after transformation of the series
    * @authors Bolshakov M.P., Pashkov B.B.
    * @param n The number of terms
    * @param order The order of the transformation
    * @return The transformed partial sum
    */
-	virtual T operator()(K n, K order) const = 0;
+    virtual T operator()(K n, K order) const = 0;
 
 protected:
-	/**
+    /**
    * @brief Series whose convergence is being accelerated
    * @authors Bolshakov M.P.
    */
-	series_templ series;
+    series_templ series;
 };
 
-template <std::floating_point T, std::unsigned_integral K, typename series_templ>
-series_acceleration<T, K, series_templ>::series_acceleration(const series_templ& series) : series(series) {}
+template<std::floating_point T, std::unsigned_integral K, typename series_templ>
+series_acceleration<T, K, series_templ>::series_acceleration(const series_templ &series) : series(series) {}
 
-template <std::floating_point T, std::unsigned_integral K, typename series_templ>
+template<std::floating_point T, std::unsigned_integral K, typename series_templ>
 constexpr void series_acceleration<T, K, series_templ>::print_info() const
 {
-	std::cout << "transformation: " << typeid(*this).name() << '\n';
+    std::cout << "transformation: " << typeid(*this).name() << '\n';
 }
