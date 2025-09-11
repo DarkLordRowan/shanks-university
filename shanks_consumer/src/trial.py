f07e647aa07bfbc15959077d82ae8bbb00007da4
abyssal <abyssal>
2025-09-12 01:39:17 +0300

    csv support, updated trial setup, removed unwanted bloat parameters

--- shanks_consumer/src/trial.py
+++ shanks_consumer/src/trial.py
@@ -1,8 +1,6 @@
 from src.params import (
     BaseAccelParam,
     BaseSeriesParam,
-    TrialParameters,
-    SeriesType,
 )
 
 import itertools
@@ -29,53 +27,53 @@ class SimpleTrial:
         self.results: list[TrialResult] = []
 
     def execute(self):
-        match self.series.series_type:
-            case SeriesType.SYNTHETIC:
-                if not (
-                    self.series.arguments
-                    and self.accel.n_values
-                    and self.accel.m_values
-                ):
-                    raise ValueError
-                for argument, n_value, m_value in itertools.product(
-                    self.series.arguments,
-                    self.accel.n_values,
-                    self.accel.m_values,
-                ):
-                    ready_series = self.series.executable(argument)  # type: ignore
-                    error, result = None, None
-                    try:
-                        result = self.accel.executable(
-                            ready_series, **self.accel.additional_args
-                        )(n_value, m_value)
-                    except Exception as e:  # TODO more debug info
-                        error = str(e)
+        if not (
+            self.series.arguments
+            and self.accel.n_values
+            and self.accel.m_values
+        ):
+            raise ValueError
+        for argument, n_value, m_value in itertools.product(
+            self.series.arguments,
+            self.accel.n_values,
+            self.accel.m_values,
+        ):
+            ready_series = self.series.executable(argument)  # type: ignore
+            error, result = None, None
+            try:
+                result = self.accel.executable(
+                    ready_series, **self.accel.additional_args
+                )(n_value, m_value)
+            except Exception as e:  # TODO more debug info
+                error = str(e)
 
-                    self.results.append(
-                        TrialResult(
-                            series_name=self.series.series_name,
-                            series_argument=argument,
-                            accel_name=self.accel.accel_name,
-                            n_value=n_value,
-                            m_value=m_value,
-                            result=result,
-                            error=error,
-                        )
-                    )
-            case SeriesType.NATURAL:
-                raise NotImplementedError
-            case _:
-                raise ValueError
+            self.results.append(
+                TrialResult(
+                    series_name=self.series.series_name,
+                    series_argument=argument,
+                    accel_name=self.accel.accel_name,
+                    n_value=n_value,
+                    m_value=m_value,
+                    result=result,
+                    error=error,
+                )
+            )
 
 
 class ComplexTrial:
-    def __init__(self, params: TrialParameters):
-        self.params = params
+
+    def __init__(
+        self,
+        series_params: list[BaseSeriesParam],
+        accel_params: list[BaseAccelParam],
+    ):
+        self.series_params = series_params
+        self.accel_params = accel_params
         self.results: list = []
 
     def execute(self):
         for series, accel in itertools.product(
-            self.params.series, self.params.methods
+            self.series_params, self.accel_params
         ):
             st = SimpleTrial(series, accel)
             st.execute()
