e7863a60a40069669cdffe8613982d37a316228d
abyssal <abyssal>
2025-09-12 16:16:48 +0300

    export json csv

--- shanks_consumer/main.py
+++ shanks_consumer/main.py
@@ -3,14 +3,9 @@ import pathlib
 from src.params import (
     AccelParamModule,
     SeriesParamModule,
-    get_series_params_from_csv,
-    get_series_params_from_json,
-    get_accel_params_from_json,
 )
 from src.trial import ComplexTrial
-from dataclasses import asdict
-import json
-
+from src.export import ExportTrialResult
 
 import pyshanks
 
@@ -26,10 +21,7 @@ if __name__ == "__main__":
             AccelParamModule(pyshanks.ShanksAlgorithm, range(10, 20), [4]),
         ],
     )
-    print(
-        json.dumps(
-            [asdict(result) for result in st.execute()],
-            indent=4,
-            sort_keys=True,
-        )
-    )
+    exporter = ExportTrialResult(st.execute())
+
+    exporter.to_json(pathlib.Path("output.json"))
+    exporter.to_csv(pathlib.Path("output.csv"))
