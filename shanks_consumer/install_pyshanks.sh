57c9fa5ed4516bef004c76453a6b623062116659
abyssal <abyssal>
2025-09-14 11:39:05 +0300

    agnostic install_pyshanks

--- shanks_consumer/install_pyshanks.sh
+++ shanks_consumer/install_pyshanks.sh
@@ -1,3 +1,2 @@
-cmake ../shanks_transformation
-cmake --build ../shanks_transformation --target pyshanks 
-cp ../shanks_transformation/*.so ../shanks_consumer/pyshanks/
+cmake ../shanks_transformation -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=../shanks_consumer/pyshanks
+cmake --build ../shanks_transformation --target pyshanks
