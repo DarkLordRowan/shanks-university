407c0ca0b10a64af771b9d55a8c4b4d3b57371a4
abyssal <abyssal>
2025-09-14 11:24:38 +0300

    cmake only install script

--- shanks_consumer/install_pyshanks.sh
+++ shanks_consumer/install_pyshanks.sh
@@ -1,4 +1,3 @@
-cd ../shanks_transformation
-cmake .
-make pyshanks
-cp pyshanks.cpython-313-x86_64-linux-gnu.so ../shanks_consumer/pyshanks/
+cmake ../shanks_transformation
+cmake --build ../shanks_transformation --target pyshanks 
+cp ../shanks_transformation/pyshanks.cpython-313-x86_64-linux-gnu.so ../shanks_consumer/pyshanks/
