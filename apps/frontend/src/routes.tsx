5985c75153b70d8a0ac8725340eb78ad35cda2cd
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-11-06 16:09:40 +0300

    Apps. Frontend. Experiments_v4

--- apps/frontend/src/routes.tsx
+++ apps/frontend/src/routes.tsx
@@ -12,6 +12,7 @@ const SeriesPage = lazy(() => import("./pages/Series/[id]"));
 const NotFound = lazy(() => import("./pages/NotFound"));
 const Experiments_v2 = lazy(() => import("./pages/Experiments_v2"));
 const Experiments_v3 = lazy(() => import("./pages/Experiments_v3"));
+const Experiments_v4 = lazy(() => import("./pages/Experiments_v4"));
 
 
 export const routes: RouteObject[] = [
@@ -23,6 +24,7 @@ export const routes: RouteObject[] = [
             {path: "experiments", element: <Experiments/>},
             {path: "experiments_v2", element: <Experiments_v2/>},
             {path: "experiments_v3", element: <Experiments_v3/>},
+            {path: "experiments_v4", element: <Experiments_v4/>},
             {path: "algorithms", element: <Algorithms/>},
             {path: "algorithms/:id", element: <Algorithm/>},
             {path: "series", element: <SeriesList/>},
