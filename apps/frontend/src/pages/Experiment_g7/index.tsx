1d51be77aa971f25a2c75e695ee0e7b53588f6fe
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-11-21 02:09:41 +0300

    Apps. Frontend. Parquet

--- apps/frontend/src/pages/Experiment_g7/index.tsx
+++ apps/frontend/src/pages/Experiment_g7/index.tsx
@@ -1,10 +1,9 @@
-import type { Experiment } from "@/types/experiment";
-import { useState } from "react";
 import { ParquetFolderInput } from "@/features/import-experiment-from-parquet/ui/ParquetFolderInput.tsx";
 import { AlgorithmSeriesDiffHeatmap } from "@/widgets/AlgorithmSeriesDiffHeatmap.tsx";
+import { usePersistedExperiment } from "@/shared/lib/hooks/usePersistedExperiment.ts";
 
 export default function Experiment_g7() {
-    const [experiment, setExperiment] = useState<Experiment | null>(null);
+    const [experiment, setExperiment] = usePersistedExperiment();
 
     return (
         <div className="mx-auto">
