1561c1e2fda9887579774e45c51d509e711bcbf9
ArkadiyShevyrov <arkadiy.shevyrov@gmail.com>
2025-11-21 02:09:41 +0300

    Apps. Frontend. Parquet

--- apps/frontend/src/pages/Experiment_g6/index.tsx
+++ apps/frontend/src/pages/Experiment_g6/index.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
 import { ParquetFolderInput } from "@/features/import-experiment-from-parquet/ui/ParquetFolderInput.tsx";
-import type { Experiment } from "@/types/experiment.ts";
 import { AlgorithmSeriesErrorMatrix } from "@/widgets/AlgorithmSeriesErrorMatrix";
+import { usePersistedExperiment } from "@/shared/lib/hooks/usePersistedExperiment";
 
 export default function Experiment_g6() {
 
-    const [experiment, setExperiment] = useState<Experiment | null>(null);
+    const [experiment, setExperiment] = usePersistedExperiment();
 
     return (
         <div className="mx-auto">
--- apps/frontend/src/pages/Experiment_g8/index.tsx
+++ apps/frontend/src/pages/Experiment_g8/index.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import { ErrorMatrixTable } from "@/widgets/ErrorMatrixTable.tsx";
-import type { Experiment } from "@/types/experiment.ts";
 import { ParquetFolderInput } from "@/features/import-experiment-from-parquet/ui/ParquetFolderInput.tsx";
+import { usePersistedExperiment } from "@/shared/lib/hooks/usePersistedExperiment";
 
 export default function Experiment_g6() {
-    const [experiment, setExperiment] = useState<Experiment | null>(null);
+    const [experiment, setExperiment] = usePersistedExperiment();
 
     return (
         <div className="mx-auto">
