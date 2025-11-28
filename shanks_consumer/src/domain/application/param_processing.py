a64ea87053c125f81b3ff3ec15e5f4005e703e09
abyssal <ilya.yadr.mihal@gmail.com>
2025-11-28 21:46:53 +0300

    float range hotfix

--- shanks_consumer/src/domain/application/param_processing.py
+++ shanks_consumer/src/domain/application/param_processing.py
@@ -1,42 +1,36 @@
 from typing import Any, Iterable
 
-
-def ensure_number(x: Any) -> Any:
-    if isinstance(x, bool):
-        return x
-    if isinstance(x, (int, float)):
-        return x
-    if isinstance(x, str):
-        try:
-            if any(sep in x for sep in [".", "e", "E"]):
-                return float(x)
-            return int(x)
-        except ValueError:
-            return x
-    try:
-        return float(x)
-    except Exception:
-        return x
-
-
 def generate_range(start: float, stop: float, step: float) -> Iterable[float]:
     if step == 0:
-        raise ValueError("step cannot be zero")
+        raise ValueError("Step cannot be zero in a range construction")
     count = int((stop - start) / step)
     return (start + i * step for i in range(count))
-
 
 def autowrap(value: Any):
     if value is None:
         return []
     if isinstance(value, dict) and {"start", "stop", "step"} <= value.keys():
-        return range(
-            ensure_number(value["start"]),
-            ensure_number(value["stop"]),
-            ensure_number(value["step"]),
-        )
+        try:
+            return generate_range(
+                float(value["start"]),
+                float(value["stop"]),
+                float(value["step"]),
+            )
+        except KeyError as e:
+            raise KeyError(
+                f"Please provide every field in a range construction: {value}"
+            ) from e
+        except ValueError as e:
+            raise ValueError(
+                f"Float is expected in a range construction: {value}"
+            ) from e
     if isinstance(value, (str, bytes, bytearray)):
         return [value]
     if isinstance(value, Iterable) and not isinstance(value, (str, bytes, dict)):
-        return [ensure_number(v) for v in value]
-    return [ensure_number(value)]
+        return value
+    try:
+        return [float(value)]
+    except ValueError as e:
+        raise ValueError(
+            f"Unknown value type failed to be converted to a float: {value}"
+        ) from e
