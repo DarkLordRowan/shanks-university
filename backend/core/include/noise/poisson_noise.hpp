b33840c4a9e058de22f886a1fba8d299b64645f3
Venajalainen <alex2004naumov@gmail.com>
2026-03-05 19:03:47 +0300

    get rid of static_assert

--- backend/core/include/noise/poisson_noise.hpp
+++ backend/core/include/noise/poisson_noise.hpp
@@ -2,11 +2,7 @@
 #define POISSSON_NOISE_HPP
 
 template <typename T>
-struct poisson_noise {
-    static T generate(const double lambda, std::mt19937_64& rng) {
-        static_assert(std::false_type{}, "poisson_noise::generate isn't implemented for this type");
-    }
-};
+struct poisson_noise;
 
 template <typename T>
     requires FloatLike<T> || IntervalLike<T>
